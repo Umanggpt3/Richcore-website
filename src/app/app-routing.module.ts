e04c760de5c9d8df219a4742e2ece9b3fe6351c1
dheeraj2596 <dheerajsaraogi25@gmail.com>
2019-09-01 15:00:25 +0530

    route to about us added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about-us/about/about.component';
+import { BannerComponent } from './banner/banner.component';
 
 const routes: Routes = [
+  {
+    path:'',component:BannerComponent
+  },
+
   {
       path: 'about',
       component: AboutComponent
   }
+
 ];
 
 @NgModule({
