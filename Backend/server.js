a72a4da564f360a737e3e4e8d860aff3db5fe504
chandansr9595 <44464633+chandansr9595@users.noreply.github.com>
2019-11-06 09:03:57 +0530

    Update server.js
    changed the server port number

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app/app');
-const port = process.env.PORT || 1025;
+const port = process.env.PORT || 1035;
 
 const server = http.createServer(app);
 if (server) {
